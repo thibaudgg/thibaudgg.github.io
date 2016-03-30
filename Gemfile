6f805576a21bbad9e47d442100879c8e567b4721
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2016-03-30 22:26:05 +0200

    Update middleman & friends
    Remove Google Analytics

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'middleman'
+gem 'middleman-sprockets', '4.0.0.rc.1'
 gem 'middleman-livereload'
 gem 'middleman-favicon-maker'
-gem 'middleman-deploy'
+gem 'middleman-gh-pages'
--- config.rb
+++ config.rb
@@ -1,62 +1,11 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
 activate :livereload
 
-# https://github.com/tvaughan/middleman-deploy
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.branch = 'master'
-  deploy.build_before = true
-  deploy.clean = false
-end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+activate :relative_assets
+set :relative_links, true
 
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'img'
 
 # Build-specific configuration
@@ -72,10 +21,4 @@ configure :build do
 
   # Enable cache buster
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
